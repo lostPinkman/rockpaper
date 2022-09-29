fdaf5b8912d0aac188d6952e10ab96d522b96679
lostPinkman <pinkmanlost@gmail.com>
2022-09-29 21:54:35 +0300

    Added first interface visual and simple css animations

--- javascript.js
+++ javascript.js
@@ -4,37 +4,99 @@ function getComputerChoice(){
     return choice;
 }
 
+const image = document.querySelector('.image')
+image.style.cssText = 'scale: 0;';  
+const audio = document.querySelector('audio')
+// const imagelost = document.querySelector('.imagelost')
+// imagelost.style.cssText = 'scale: 0;';  
+
+
+
+const buttons = document.querySelectorAll('button')
+const div = document.querySelector('.divs')
+// let totalGames = 0;
+let Yourscore = 0;
+let Compscore = 0;
+
+buttons.forEach(button => button.addEventListener('click', playRound))
+
 
 function playRound(playerSelection, computerSelection=getComputerChoice()) {
-    playerSelection = prompt('Your turn')
+    audio.volume = 0.5
+    audio.currentTime = 0.35;
+    audio.play()
+    while (Yourscore <5 && Compscore <5 ) {
+    playerSelection = this.className
+    console.log(playerSelection)
     
     if (playerSelection === computerSelection) {    
+        image.style.cssText = 'scale: 0;';
+        div.textContent = `Draw! Play again! Your score is: ${Yourscore}
+        Computer score is: ${Compscore}`
         return "Draw! Play again"
+        
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+      
+        image.style.cssText = 'scale: 1;';
+        Yourscore += 1;
+        div.textContent = `You Won! Your score is: ${Yourscore}
+        Computer score is: ${Compscore}`
         return 'You Won!'   
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+     
+        image.style.cssText = 'scale: 0;';
+        Compscore += 1;
+        div.textContent = `You Lost! Play again! Your score: is ${Yourscore}
+        Computer score is: ${Compscore}`
         return 'You Lost!'
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'You Won!'
+     
+        image.style.cssText = 'scale: 1;';
+        Yourscore += 1;
+        div.textContent = `You Won! Your score is: ${Yourscore}
+        Computer score is: ${Compscore}`
+        return 'You Won!' 
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+     
+        image.style.cssText = 'scale: 0;';
+        Compscore += 1;
+        div.textContent = `You Lost! Play again! Your score is: ${Yourscore}
+        Computer score is: ${Compscore}`
         return 'You Lost!'
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+     
+        image.style.cssText = 'scale: 0;';
+        Compscore += 1;
+        div.textContent = `You Lost! Play again! Your score is: ${Yourscore}
+        Computer score is: ${Compscore}`
         return 'You Lost!'
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'You Won!'
+        
+        image.style.cssText = 'scale: 1;';
+        Yourscore += 1;
+        div.textContent = `You Won! Your score is: ${Yourscore}
+        Computer score is: ${Compscore}`
+        return 'You Won!' 
     }
+}   if (Yourscore > Compscore) alert(`You Won this game! 
+Your Score is: ${Yourscore}`)
+    else {
+        alert(`You Lost this game! Computer Score is: ${Compscore}`)
+    }
+console.log(score)
 }
 
-let score = 0;
-function game() {
-    for (let i = 0; i < 5; i++) {
-        if (playRound() === 'You Won!') {
-            score +=1;
-            console.log('This round: WON!')
-        } else console.log('This round you Lost')
-        } const result = score >= 3 ? 'You won game, congratulations!' : 'You lost :(';
-        return result   
-    }
+
+// let score = 0;
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         if (playRound() === 'You Won!') {
+//             score +=1;
+//             div.textContent = `Your score is ${score}`
+//         } else console.log('This round you Lost')
+//         } const result = alert(score >= 3 ? 'You won game, congratulations!' : 'You lost :(');
+//         return result   
+//     }
 
 
 // const playerSelection = "rock";
