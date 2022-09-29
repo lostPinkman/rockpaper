be33742fdd2978967a734e7cdca498c420db5d48
lostPinkman <pinkmanlost@gmail.com>
2022-09-29 16:14:51 +0300

    trying delete

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function repeatString(string, num ) {
-    let total = '';
-    const result = function() {
-        for( let i = 0; i < num; i++) {
-             total += string
-        }
-    }     
-}
